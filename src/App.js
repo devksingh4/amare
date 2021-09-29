f9944429dde562e7aa171e324f2f3e033b8f34b5
Dev Singh <dev@devksingh.com>
2021-09-29 09:58:54 -0500

    push new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { getApp } from './firebase'
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getFirestore, getDocs, collection } from 'firebase/firestore';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,11 +12,39 @@ export default class App extends React.Component {
       counter: 1
     }
     this.incrementCounter = this.incrementCounter.bind(this);
+    this.addResponse = this.addResponse.bind(this);
+  }
+  componentDidMount() {
+    this.addResponse();
+  }
+  incrementCounter() {
+    this.setState({ counter: this.state.counter += 1 })
   }
 
-  incrementCounter() {
-    this.setState({counter: this.state.counter += 1})
+  async addResponse() {
+    console.log("h")
+    const app = getApp();
+    const db = getFirestore()
+    const auth = getAuth();
+    signInAnonymously(auth)
+      .then(async () => {
+        const querySnapshot = await getDocs(collection(db, "responses"));
+        querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => ${doc.data()}`);
+        });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ...
+      });
+
+    // const res = await db.collection('responses').add({
+    //   response: "Test 123"
+    // })
+    // console.log(res)
   }
+
 
   render() {
 
@@ -32,8 +63,8 @@ export default class App extends React.Component {
           >
             Learn React
           </a>
-          <button onClick={this.incrementCounter}>
-            Increment Counter
+          <button onClick={this.addResponse}>
+            Add to Firebase
           </button>
         </header>
 
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
+
+export function getApp() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyA5PWfwZi3KJY_mLFjbSyxDRNN1P6o2q08",
+        authDomain: "amare-650e9.firebaseapp.com",
+        projectId: "amare-650e9",
+        storageBucket: "amare-650e9.appspot.com",
+        messagingSenderId: "549313195564",
+        appId: "1:549313195564:web:3915c2d973cad306c0eb89"
+    };
+
+    return initializeApp(firebaseConfig);
+}
